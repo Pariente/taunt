b3f6b24318e95f9b0ac8028a4b226518e2e1f742
pariente <parientethomas@gmail.com>
2015-12-03 14:15:39 +0100

    social fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,22 +1,4 @@
-var facebookShare = document.querySelector('[data-js="facebook-share"]');
-var twitterShare = document.querySelector('[data-js="twitter-share"]');
 var client = new ZeroClipboard(document.getElementById("copy-button"));
-
-// Facebook
-facebookShare.onclick = function(e) {
-e.preventDefault();
-var facebookWindow = window.open('http://www.facebook.com/sharer/sharer.php?u=https%3A//taunt.io/taunt/{{ taunt.id }}&title=Taunt', 'facebook-popup', 'height=350,width=600');
-if(facebookWindow.focus) { facebookWindow.focus(); }
-  return false;
-}
-
-// Twitter
-twitterShare.onclick = function(e) {
-  e.preventDefault();
-  var twitterWindow = window.open('https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Ftaunt.io&url=' + document.URL + '&hashtags=taunt&via=taunt_io&text={{ taunt.title }}%20%2D', 'twitter-popup', 'height=350,width=600');
-  if(twitterWindow.focus) { twitterWindow.focus(); }
-    return false;
-  }
 
 // Copy URL to clipboard (ZeroClipboard)
 client.on("ready", function(readyEvent) {
